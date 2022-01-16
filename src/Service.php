712fe9a0919d2bdd5854228ca447ea9c2edacd4c
Tian Khamez <tkhamez@gmail.com>
2022-01-16 13:26:49 +0100

    Update tkhamez/neucore-plugin.

--- src/Service.php
+++ src/Service.php
@@ -450,7 +450,8 @@ class Service implements ServiceInterface
         CoreCharacter $coreCharacter,
         string $name,
         ServerRequestInterface $request,
-        ResponseInterface $response
+        ResponseInterface $response,
+        array $groups
     ): ResponseInterface {
         throw new Exception();
     }
