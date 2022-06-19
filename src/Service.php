d7d58222d573939754ddea4129907b32dd8bb424
Tian Khamez <tkhamez@gmail.com>
2022-06-19 17:07:13 +0200

    Update tkhamez/neucore-plugin.

--- src/Service.php
+++ src/Service.php
@@ -201,6 +201,11 @@ class Service implements ServiceInterface
         throw new Exception();
     }
 
+    public function moveServiceAccount(int $toPlayerId, int $fromPlayerId): bool
+    {
+        return true;
+    }
+
     /**
      * @throws Exception
      */
