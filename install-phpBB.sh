c0ca365e19f923a294ec5859e487cc58a7740b25
Tian Khamez <tkhamez@gmail.com>
2024-08-08 21:23:30 +0200

    Adjust phpbb install script.

--- install-phpBB.sh
+++ install-phpBB.sh
@@ -5,19 +5,14 @@ DIR=$(dirname "$(realpath "$0")")
 FILE=phpBB-3.3.4.tar.bz2
 URL=https://download.phpbb.com/pub/release/3.3/3.3.4/"$FILE"
 
-if [[ ! -f "${DIR}"/"$FILE" ]]; then
-  # Amazon Linux does not like the certificate from Let's Encrypt
-  wget --no-check-certificate $URL -O "${DIR}"/"$FILE"
-fi
-if [[ ! -f "${DIR}"/"$FILE" ]]; then
-  echo Error: failed to download $URL
-  exit 1
+if [ ! -f "${DIR}"/"$FILE" ]; then
+    wget -nv $URL -O "${DIR}"/"$FILE" || exit 1
 fi
 
-if [[ -d "${DIR}"/phpBB3 ]]; then
-  rm -R "${DIR}"/phpBB3
+if [ -d "${DIR}"/phpBB3 ]; then
+    rm -R "${DIR}"/phpBB3
 fi
+tar jxf "${DIR}"/"$FILE" -C "${DIR}" || exit 1
 
-tar jxf "${DIR}"/"$FILE" -C "${DIR}"
 chmod 0777 "${DIR}"/phpBB3/config.php
 chmod 0777 "${DIR}"/phpBB3/config
